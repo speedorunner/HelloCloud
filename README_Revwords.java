cc65d1153e259dc8016ce5c3a759e58297f2c724
speedorunner <32364828+speedorunner@users.noreply.github.com>
2017-09-28 14:07:27 -0700

    README added

---
+++ README_Revwords.java
@@ -0,0 +1,45 @@
+
+//************************************************************************************************************************
+// DNU 09-27-2017
+// Program Name: Revwords.java
+//
+// Description: The Revwords.java application returns strings in the reverse order in which they were written.
+//
+// Methods: void main(String args[]), String transpose(String word)
+//
+// Class Variable/Objects: StringBuffer result
+// 
+// Test Scenarios: see reports.
+// 
+//************************************************************************************************************************
+public class Revwords {
+    
+    public static void main(String args[]) 
+    {
+    	// Test phrase to be reversed here.  This can be replaced with actual screen input.
+        Revwords obj = new Revwords();
+        String tstphrase = "987654321";
+        
+    //test to display original message.
+        System.out.println("The Original message is:" + tstphrase);   
+    //test method to catch reversal..add try/catch for err handling.
+        System.out.println("The New message is:" + obj.transpose(tstphrase));
+    }
+//************************************************************************************************************************
+// transpose(String): This method accepts a string and if there is more than one character in the string will return the string 
+// characters in reverse order.  
+//************************************************************************************************************************
+    //Class variable to store reversed string.
+    private StringBuffer result = new StringBuffer();
+    
+    public String transpose(String word){
+        
+	//Initially, the method has a limit constraint set so when the cursor’s index is the first character it will exit the method.
+        
+	//Next, it recursively calls the transpose() method and appends the characters in reverse order until the last character
+	//which is handled by the first ‘if’ condition.  The string is then returned for screen output.
+
+        }
+    }
+    
+}
